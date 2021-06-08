9bd08cb0bfb63b9832706cccf6412b3d0018a6d6
roma2121 <82339399+roma2121@users.noreply.github.com>
2021-06-08 15:06:06 +0300

    Add files via upload

---
+++ matrices.c
@@ -0,0 +1,77 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "matrices.h"
+
+void mx_free(TMatrix *m) {
+ if (m != NULL)
+  if (m->a != NULL) {
+   free(m->a);
+   free(m);
+  }
+}
+
+void mx_print(TMatrix *m) {
+ unsigned int i, j;
+
+ if (m != NULL) {
+  if (m->a != NULL) {
+   for (i = 0; i < m->m; i++) {
+    for (j = 0; j < m->n; j++)
+     printf("%8.2lf", m->a[i * m->n + j]);
+    printf("\n");
+   }
+  }
+ }
+}
+
+TMatrix *mx_identity(unsigned int n) {
+ unsigned int i, j;
+ TMatrix *m = NULL;
+
+ m = (TMatrix*)malloc(sizeof(TMatrix));
+ if (m != NULL) {
+  m->a = (double*)malloc(n * n * sizeof(double));
+  if (m->a != NULL) {
+   m->n = m->m = n;
+   for (i = 0; i < m->m; i++)
+    for (j = 0; j < m->n; j++)
+     m->a[i * n + j] = i == j ? 1 : 0;
+  }
+  else {
+   free(m);
+   m = NULL;
+  }
+ }
+ return m;
+}
+
+/**
+Sort with a snake
+*/
+void mx_sort(TMatrix *m)
+{
+ // Sorting the linearized matrix
+ for (int i = 0; i + 1 < m->m * m->n; ++i)
+ {
+  for (int j = i + 1; j < m->m * m->n; ++j)
+  {
+   if (m->a[i] > m->a[j])
+   {
+    double tmp = m->a[i];
+    m->a[i] = m->a[j];
+    m->a[j] = tmp;
+   }
+  }
+ }
+
+ // Invert odd rows
+ for (int i = 1; i < m->m; i += 2)
+ {
+  for (int j = 0; j < m->n / 2; ++j)
+  {
+   double tmp = m->a[j + i * m->n];
+   m->a[j + i * m->n] = m->a[m->n - 1 - j + i * m->n];
+   m->a[m->n - 1 - j + i * m->n] = tmp;
+  }
+ }
+}
---
+++ matrices.h
@@ -0,0 +1,68 @@
+#ifndef __MATRIX__
+#define __MATRIX__
+
+typedef struct {
+	//number of rows in the data array
+	unsigned int    m;
+	//number of columns in the data array
+	unsigned int    n;
+	//reference to an array with data
+	double          *a;
+} TMatrix;
+
+/**
+Frees up memory
+*/
+void mx_free(TMatrix *m);
+
+/**
+Displays the matrix m
+*/
+void mx_print(TMatrix *m);
+
+/**
+Returns a unit matrix of size n
+*/
+TMatrix *mx_identity(unsigned int n);
+
+/**
+Returns a matrix created from a string, where the string elements are separated by a space, and the strings are separated by a straight slash.
+*/
+TMatrix *mx_from_text(const char *s);
+
+/**
+Returns the result of multiplying m1 and m2
+*/
+TMatrix *mx_mul(TMatrix *m1, TMatrix *m2);
+
+/**
+Returns the result of adding m1 and m2
+*/
+TMatrix *mx_add(TMatrix *m1, TMatrix *m2);
+
+/**
+Returns the result of multiplying the matrix m by the scalar s
+*/
+TMatrix *mx_mul_scalar(TMatrix *m, double s);
+
+/**
+Returns the result of raising the matrix m to the power of s
+*/
+TMatrix *mx_pow(TMatrix *m, double s);
+
+/**
+Returns the result of transposing the matrix m
+*/
+TMatrix *mx_transponse(TMatrix *m);
+
+/**
+Returns the determinant of the matrix m
+*/
+double mx_det(TMatrix *m);
+
+/**
+Sort by the 1st line
+*/
+void mx_sort(TMatrix *m);
+
+#endif
